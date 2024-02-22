35bc5e9c47b02060e936f8718a79e3987b2a751b
Илья Нестеров <88728202+ilyhan@users.noreply.github.com>
2024-02-22 22:53:05 +0300

    test

--- js/scrollNav.js
+++ js/scrollNav.js
@@ -7,3 +7,31 @@ window.onscroll = function (e) {
         nav.classList.remove("none");
     }
 };
+
+const nodemailer = require('nodemailer');
+
+// Создать объект transporter для отправки писем
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'i04534748@gmail.com',
+    pass: 'ThePassword'
+  }
+});
+
+// Определить содержимое и настройки письма
+let mailOptions = {
+  from: 'i04534748@gmail.com',
+  to: 'ilya.nesterov2323@example.com',
+  subject: 'Тема сообщения',
+  text: 'Текст сообщения'
+};
+
+// Отправить письмо
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
